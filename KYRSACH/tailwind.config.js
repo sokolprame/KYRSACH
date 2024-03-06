565df958c650246579a46c24dd829d49a21e1c82
ЯРик вадила < >
2024-03-06 09:09:31 +0300

    0.1

---
+++ KYRSACH/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './src/**/*.html',
+        './src/**/*.js',
+    ],
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
