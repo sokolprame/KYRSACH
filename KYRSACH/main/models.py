81cfbac894c4411398867c4f1a331adda05a6799
ЯРик вадила < >
2024-03-25 17:10:50 +0300

    ЭВРИКА нах

--- KYRSACH/main/models.py
+++ KYRSACH/main/models.py
@@ -5,7 +5,8 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
-
+    color = models.CharField(max_length=50)
+    size = models.CharField(max_length=10)
+    
     def __str__(self):
         return self.title
