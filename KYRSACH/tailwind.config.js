4c30ba6bac82731185960e67a6034fbfad982d9f
Sokolprame <kstrin14t27@mail.ru>
2024-05-01 11:20:02 +0300

    Шрифт

--- KYRSACH/tailwind.config.js
+++ KYRSACH/tailwind.config.js
@@ -5,43 +5,44 @@ module.exports = {
         './src/**/*.js',
     ],
     theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
-    theme: {
-        screens: {
-            sm: '480px',
-            md: '768px',
-            lg: '976px',
-            xl: '1440px',
-        },
-        colors: {
-            'blue': '#1fb6ff',
-            'purple': '#7e5bef',
-            'pink': '#ff49db',
-            'orange': '#ff7849',
-            'green': '#13ce66',
-            'yellow': '#ffc82c',
-            'gray-dark': '#273444',
-            'gray': '#8492a6',
-            'gray-light': '#d3dce6',
-        },
-        fontFamily: {
-            sans: ['Graphik', 'sans-serif'],
-            serif: ['Merriweather', 'serif'],
-        },
         extend: {
-            spacing: {
-                '128': '32rem',
-                '144': '36rem',
+            screens: {
+                sm: '480px',
+                md: '768px',
+                lg: '976px',
+                xl: '1440px',
             },
-            borderRadius: {
-                '4xl': '2rem',
-            }
-        }
+            colors: {
+                'blue': '#1fb6ff',
+                'purple': '#7e5bef',
+                'pink': '#ff49db',
+                'orange': '#ff7849',
+                'green': '#13ce66',
+                'yellow': '#ffc82c',
+                'gray-dark': '#273444',
+                'gray': '#8492a6',
+                'gray-light': '#d3dce6',
+            },
+            fontFamily: {
+               'font-face': ['Rubik_Wet_Paint'],
+                sans: ['Graphik', 'sans-serif'],
+                serif: ['Merriweather', 'serif'],
+            },
+            extend: {
+                spacing: {
+                    '128': '32rem',
+                    '144': '36rem',
+                },
+                borderRadius: {
+                    '4xl': '2rem',
+                }
+            },
+        },
+        variants: {
+            extend: {},
+        },
+        plugins: [],
 
 
+    }
 }
