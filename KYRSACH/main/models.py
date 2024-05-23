0b89afa986e2917e330805187481536e58dbe73e
sokolprame <kstrin14t27@mail.ru>
2024-05-23 11:44:40 +0300

    0.2.2.1

--- KYRSACH/main/models.py
+++ KYRSACH/main/models.py
@@ -1,3 +1,6 @@
+from ctypes.wintypes import SIZE
+from pyexpat import model
+from turtle import color
 from unittest.util import _MAX_LENGTH
 from django.db import models
 
@@ -10,6 +13,12 @@ class Product(models.Model):
     size = models.CharField(max_length=10)
 
 class UserUpdate(models.Model):
-    
+    title = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    prise = models.DecimalField(max_digits=10, decimal_places=2)
+    color = models.CharField(max_length=50)
+    size = models.CharField(max_length=10)
+
     def __str__(self):
         return f'{self.title} {self.size}'
