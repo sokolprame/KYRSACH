2b6eb267d385020361c253707a5de6f027b3960e
Sokolprame <kstrin14t27@mail.ru>
2024-06-26 21:53:40 +0300

    формч

--- KYRSACH/main/forms.py
+++ KYRSACH/main/forms.py
@@ -1,14 +1,20 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
 from goods.models import Order
 
 from django import forms
 
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Username'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Password'}))
-
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Username'
+    }))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'class': 'form-control',
+        'placeholder': 'Password'
+    }))
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
