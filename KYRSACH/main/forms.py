35ed5f6bd7017a1e8394e30cd6da6418c90d879f
Sokolprame <kstrin14t27@mail.ru>
2024-06-26 21:31:02 +0300

    проба

--- KYRSACH/main/forms.py
+++ KYRSACH/main/forms.py
@@ -3,6 +3,13 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from goods.models import Order
 
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150, widget=forms.TextInput(attrs={'class': 'form-input', 'placeholder': 'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-input', 'placeholder': 'Password'}))
+
+
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
 
@@ -14,3 +21,9 @@ class PurchaseForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['address', 'card_number', 'expiration_date', 'cvv']
+        widgets = {
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'card_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'expiration_date': forms.TextInput(attrs={'class': 'form-control'}),
+            'cvv': forms.TextInput(attrs={'class': 'form-control'}),
+}
--- KYRSACH/main/views.py
+++ KYRSACH/main/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import LoginForm
+
+
 
 def home(request):
     return render(request, 'main/home.html')
@@ -10,26 +13,25 @@ def register(request):
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=password)
-            login(request, user)
-            return redirect('home')
+            return redirect('login')
     else:
         form = UserCreationForm()
     return render(request, 'main/register.html', {'form': form})
 
 def login_view(request):
     if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+        form = LoginForm(request.POST or None)
         if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('index')
     else:
-        form = AuthenticationForm()
+        form = LoginForm()
     return render(request, 'main/login.html', {'form': form})
 
 def logout_view(request):
     logout(request)
-    return redirect('home')
+    return redirect('index')
